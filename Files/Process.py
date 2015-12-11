37a689914e6e000c79d2d74fa828423fc440c14d
Scott Rose <crazyhousethinker@gmail.com>
2015-12-11 10:39:11 -0800

    docstring and refactoring

--- Files/Process.py
+++ Files/Process.py
@@ -9,25 +9,28 @@ import ntpath
 class Process(object):
     '''
     Process Object is varouis file operations.
-    getAllWaves() -> Gets all waves in the wave cache.
-    copyWave() -> moves wave file to the wave cache.
-    getWaveSize() -> gets total size of waves.
+    getallwaves() -> Gets all waves in the wave cache.
+    copywave() -> moves wave file to the wave cache.
+    getwavesize() -> gets total size of waves.
     '''
     def __init__(self):
         pass
-    "GetAllWaves(path): returns list of wave files in a folder."
-    def GetAllWaves(self,path=""):
-        wavFileList = []
+
+    def getallwaves(self,path=""):
+        "GetAllWaves(path): returns list of wave files in a folder."
+        wav_file_list = []
         for (dirpath, dirnames, filenames) in os.walk(path):
             for filename in filenames:
                 if filename.endswith(".wav"):
-                    wavFileList.append(os.sep.join([dirpath, filename]))
-        return wavFileList
-    "CopyWave(filepath): copies wave file to another directory."
-    def CopyWave(self,path=""):
+                    wav_file_list.append(os.sep.join([dirpath, filename]))
+        return wav_file_list
+
+    def copywave(self,path=""):
+        "CopyWave(filepath): copies wave file to another directory."
         shutil.copyfile(path, os.path.join(os.getcwd(),"waves",ntpath.basename(path)))
-    "GetWaveSize(path): returns total size of all wave files in a directory."
-    def getWaveSize(self,path=""):
+
+    def getwavesize(self,path=""):
+        "GetWaveSize(path): returns total size of all wave files in a directory."
         total_size = 0
         start_path = path
         for dirpath, dirnames, filenames in os.walk(start_path):
--- decoder/Phreak.py
+++ decoder/Phreak.py
@@ -14,20 +14,18 @@ class VanEck(threading.Thread):
     This VanEck object gets a image frame from a wave file and returns a folder
     of files that based on frames per second and length of wave file.
      constructor: opens wave file and reads the data into an array.
-    _deltaTime(): returns change in time
-    _deltaVolume(): returns a change in amplitude of the recording
-    _deltaPoint(): returns a change in position from a step in file
-    _buildFrame(): generates a png file from the wave file.
-    run(): loop through the wav file and build each file with buildFrame().
+    _DeltaTime(): returns change in time based on refresh rate.
+    _SaveImage(): creates screen shot from run method.
+    run(): loop through the wav file and builds screen shot.
     """
     def __init__(self,**kwargs):
+        "constructor sets values for thread"
         threading.Thread.__init__(self)
         self._audio = []
         self._path = kwargs["path"]
         self._title = kwargs["title"]
         self._loggs = Files.TempestLogs(self._path)
         self._rate = kwargs["framerate"]
-        #basic height and width
         self._h = kwargs["height"]
         self._w = kwargs["width"]
         self._fProc = Files.Process()
@@ -35,21 +33,20 @@ class VanEck(threading.Thread):
         self._r = kwargs["contrastred"]
         self._g = kwargs["contrastgreen"]
         self._b = kwargs["contrastblue"]
-        #timing values.
         self._href_timing = (self._DeltaTime() / (self._w * self._rate))
         self._vref_timing = (self._DeltaTime() / self._rate)
         self._pixel_timing = (self._DeltaTime() / (self._h * self._w * self._rate))
 
-    #measure overall change in time based on system clock.
     def _DeltaTime(self):
+        "gets time difference based on refresh rate."
         seconds = 1/self._rate
         timeOne = time.clock()
         time.sleep(seconds/1000)
         timeTwo = time.clock()
         return abs(timeTwo - timeOne)
 
-    #save Image
     def _SaveImage(self,screen=[],index=0):
+        "saves image with Pillow"
         print("Saving Screenshot: " + str(index))
         pixel_out = []
         for i in range(len(screen)):
@@ -60,35 +57,24 @@ class VanEck(threading.Thread):
         im.save(os.path.join(self._path,self._title + str(index) +".bmp"))
         print("Saved: %s" % os.path.join(self._path,self._title + str(index) +".bmp"))
 
-    #gets the lines from the wave files.
-    #stores them in a scans array to run the draw method.
     def run(self):
-        #audio and drawing increment values
+        "thread override run method. writes screenshots from audio."
         pos = 0
         photo = 0
         screens = []
         line = []
-        #wave file data stored in self.audio array.
         for wav in self._fProc.GetAllWaves(self._path):
             print("Parsing Audio file: %s" % wav)
             self._loggs.WriteMsg("Parsing Audio file %s" % wav)
             f = wave.open(wav, 'r')
             for i in range(f.getnframes()):
                 for x in f.readframes(i):
-                    #individual pixel timing
-                    #shift pixel
-                    #convert
-                    #add to array of line
                     if time.clock() % self._pixel_timing != 0:
                         hue = PixelShift.HueShift(self._r,self._g,self._b,self._saturation,x)
                         line.append(hue.ColorOut())
-                    #time with horizontal refresh
-                    #append to array of screens
                     if time.clock() % self._href_timing != 0 and len(line) >= self._w:
                         screens.append(line)
                         line = []
-                    #overall screen refresh
-                    #save image
                     if time.clock() % self._vref_timing != 0 and len(screens) >= self._h:
                         self._SaveImage(screens,photo)
                         photo += 1
